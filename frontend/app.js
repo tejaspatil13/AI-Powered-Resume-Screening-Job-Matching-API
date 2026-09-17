b55e64edf238345b282a2c89c1a8912598730197
tejaspatil13 <tejaspatil92842@gmail.com>
2026-09-17 16:18:32 +0530

    backend on rendor

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://ai-powered-resume-screening-job-matching-d816.onrender.com";
+const API_BASE = "https://ai-powered-resume-screening-job-matching-d816.onrender.com";
 
 
 /* =========================================================
